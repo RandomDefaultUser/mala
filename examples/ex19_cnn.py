d5f83c1d71b5d728d76c9562a564343ce1937249
Lenz Fiedler <l.fiedler@hzdr.de>
2022-12-21 17:06:27 +0100

    Experimented with density and optimized atomic density

--- examples/ex19_cnn.py
+++ examples/ex19_cnn.py
@@ -12,11 +12,11 @@ params.data.output_rescaling_type = "normal"
 params.network.number_of_input_channels = 1
 params.network.number_of_output_channels = 22
 params.network.layer_sizes = [22, 11]
-params.network.kernel_size = 5
+params.network.kernel_size = 21
 params.descriptors.descriptors_contain_xyz = False
 params.network.nn_type = "locality-cnn"
 params.running.max_number_epochs = 500
-params.running.learning_rate = 1
+params.running.learning_rate = 2
 # params.data.data_splitting_3d = [3, 3, 3]
 
 data_handler = mala.DataHandler(params)
@@ -25,10 +25,10 @@ inpath = "/home/fiedlerl/data/mala_data_repo/Be2/" \
          "densities_gp/inputs_gaussians/"
 outpath = "/home/fiedlerl/data/mala_data_repo/Be2/training_data/"
 
-data_handler.add_snapshot("gaussians1.in.npy", inpath,
+data_handler.add_snapshot("gaussians_optimized1.in.npy", inpath,
                           "Be_snapshot1.out.npy", outpath,
                           add_snapshot_as="tr", output_units="1/(eV*Bohr^3)")
-data_handler.add_snapshot("gaussians2.in.npy", inpath,
+data_handler.add_snapshot("gaussians_optimized2.in.npy", inpath,
                           "Be_snapshot2.out.npy", outpath,
                           add_snapshot_as="va", output_units="1/(eV*Bohr^3)")
 data_handler.prepare_data()
@@ -39,8 +39,8 @@ trainer.train_network()
 
 params.data.use_lazy_loading = True
 data_handler.clear_data()
-data_handler.add_snapshot("gaussians3.in.npy", inpath,
-                          "Be_snapshot3.out.npy", outpath,
+data_handler.add_snapshot("gaussians_optimized1.in.npy", inpath,
+                          "Be_snapshot1.out.npy", outpath,
                           add_snapshot_as="te", output_units="1/(eV*Bohr^3)")
 data_handler.prepare_data(reparametrize_scaler=False)
 
@@ -48,19 +48,30 @@ tester = mala.Tester(params, network, data_handler)
 actual_ldos, predicted_ldos = tester.test_snapshot(0)
 ldos_calculator: mala.LDOS
 ldos_calculator = data_handler.target_calculator
-ldos_calculator.read_additional_calculation_data("qe.out", outpath+"Be_snapshot3.out")
+ldos_calculator.read_additional_calculation_data("qe.out", outpath+"Be_snapshot1.out")
 
 ldos_calculator.read_from_array(actual_ldos)
-print(ldos_calculator.number_of_electrons, ldos_calculator.band_energy)
-actual_dos = ldos_calculator.density_of_states.copy()
-energy_grid = ldos_calculator.energy_grid
+density_calculator = mala.Density.from_ldos_calculator(ldos_calculator)
+density_calculator.write_as_cube("actual.cube", density_calculator.density)
+
 ldos_calculator.read_from_array(predicted_ldos)
-print(ldos_calculator.number_of_electrons, ldos_calculator.band_energy)
-predicted_dos = ldos_calculator.density_of_states.copy()
-plt.plot(energy_grid, actual_dos, label="Actual DOS")
-plt.plot(energy_grid, predicted_dos, label="Predicted DOS")
-plt.legend()
-plt.show()
+density_calculator = mala.Density.from_ldos_calculator(ldos_calculator)
+density_calculator.write_as_cube("cnn.cube", density_calculator.density)
+
+gaussians = np.load(inpath+"gaussians_optimized1.in.npy").reshape([18*18*27, 1])
+density_calculator.write_as_cube("gaussians.cube", gaussians)
+
+
+# print(ldos_calculator.number_of_electrons, ldos_calculator.band_energy)
+# actual_dos = ldos_calculator.density_of_states.copy()
+# energy_grid = ldos_calculator.energy_grid
+# ldos_calculator.read_from_array(predicted_ldos)
+# print(ldos_calculator.number_of_electrons, ldos_calculator.band_energy)
+# predicted_dos = ldos_calculator.density_of_states.copy()
+# plt.plot(energy_grid, actual_dos, label="Actual DOS")
+# plt.plot(energy_grid, predicted_dos, label="Predicted DOS")
+# plt.legend()
+# plt.show()
 
 
 
